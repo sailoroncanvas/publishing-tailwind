b3479ef5190be516ef3a34dd3a547362d88a5d10
pjieuniv20 <pjieuniv20@ewhain.net>
2022-04-06 09:49:07 +0900

    initial

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
 
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="m-4">
+      <div className="mb-4 font-bold text-2xl">List</div>
+      <div className="p-4 border">
+        <div className="py-20 bg-indigo-50"></div>
+        <div className="font-bold mt-3 mb-2">Card title</div>
+        <div className="text-gray-500 font-light mb-4 text-base">
+          Sed vel turpis adipiscing penatibus orcineque. Erat sed fermentum
+          ipsum vel quis quam. Nunc etiam dui tortor, non in aluquam lacinia
+          tempor.
+        </div>
+        <div className="py-2 bg-gray-800 text-white text-center font-bold mb-3">
+          button
+        </div>
+        <div className="py-2 border-2 border-gray-800 text-gray text-center font-bold">
+          button
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import { TextField } from '../components/TextField';
 
 export const SignupPage = () => {
-  return <div>SignupPage</div>;
+  return (
+    <div className="m-4">
+      <div className="text-2xl font-bold mb-10">회원가입</div>
+      <div>
+        <TextField label="타이틀" placeholder="내용을 입력해주세요." />
+        <div className="mb-4"></div>
+        <TextField label="타이틀" placeholder="내용을 입력해주세요." />
+        <div className="mb-4"></div>
+        <TextField label="타이틀" placeholder="내용을 입력해주세요." />
+        <div className="mb-4"></div>
+        <TextField label="타이틀" placeholder="내용을 입력해주세요." />
+      </div>
+      <div>
+        <div className="border-2 border-gray-800 rounded-md text-gray-800 text-center py-4 font-semibold mt-7">
+          회원가입
+        </div>
+      </div>
+    </div>
+  );
 };
